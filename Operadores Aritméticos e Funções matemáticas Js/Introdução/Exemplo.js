12d51b83eab7e00e4e57fce3d0f116626f8bcb3f
Rudimacy Duprat <89957139+RudimacyDuprat@users.noreply.github.com>
2025-08-14 19:48:25 -0300

    Create Exemplo.js

---
+++ Operadores Aritméticos e Funções matemáticas Js/Introdução/Exemplo.js
@@ -0,0 +1,18 @@
+let num1 = 10;
+let num2 = 5;
+
+console.log("Adição:", num1 + num2); // Saída: 15
+console.log("Subtração:", num1 - num2); // Saída: 5
+console.log("Multiplicação:", num1 * num2); // Saída: 50
+console.log("Divisão:", num1 / num2); // Saída: 2
+console.log("Módulo:", num1 % num2); // Saída: 0
+
+let numero = 7.2;
+console.log("Arredondamento:", Math.round(numero)); // Saída: 7
+console.log("Arredondamento para cima:", Math.ceil(numero)); // Saída: 8
+console.log("Arredondamento para baixo:", Math.floor(numero)); // Saída: 7
+console.log("Valor absoluto:", Math.abs(-5)); // Saída: 5
+console.log("Potência:", Math.pow(2, 3)); // Saída: 8
+
+let numeroAleatorio = Math.random();
+console.log("Número aleatório:", numeroAleatorio);
